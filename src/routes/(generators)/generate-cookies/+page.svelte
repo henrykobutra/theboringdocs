c5029d464cb4e36e01e687c37f397b8d57707557
Henry Kobutra <henrykobutra@gmail.com>
2023-04-30 23:34:35 -0500

    extracted Generator Window

--- src/routes/(generators)/generate-cookies/+page.svelte
+++ src/routes/(generators)/generate-cookies/+page.svelte
@@ -1,115 +1,11 @@
 <script lang="ts">
-	import SelectBusinessType from '../SelectBusinessType.svelte';
-	import InputInfo from '../InputInfo.svelte';
-	import DownloadDoc from '../DownloadDoc.svelte';
-	import ReviewDoc from '../ReviewDoc.svelte';
+	import GeneratorWindow from '../GeneratorWindow.svelte';
 
-	let step = 1;
 	let generatorType = 'generate-cookies';
-	let businessType = '';
-	let businessName = '';
-	let businessEmail = '';
-	let policyMarkdown = '';
-
-	let nextButtonText = 'Next';
-
-	function resetForm() {
-		step = 1;
-		businessType = '';
-		businessName = '';
-		businessEmail = '';
-	}
-
-	function handleForward() {
-		if (step < 4) {
-			step++;
-		}
-	}
-
-	function handleBackward() {
-		if (step > 1) {
-			step--;
-		}
-	}
-
-	$: if (step === 1) {
-		resetForm();
-	}
-
-	$: if (businessType) {
-		handleForward();
-	}
-	$: if (businessName === '' && businessEmail === '' && step === 2) {
-		nextButtonText = 'Just Take Me To The Template';
-	} else if (step === 4) {
-		nextButtonText = 'Start Over';
-	} else {
-		nextButtonText = 'Next';
-	}
 </script>
 
 <div class="flex justify-center mt-8">
 	<div class="max-w-screen-md w-full">
-		<div class="mockup-window border bg-base-300 w-full">
-			<div class="flex flex-col justify-center py-2 bg-base-200 p-8 items-center">
-				<h1 class="font-mono font text-center text-2xl py-4">The Cookie Policy Generator</h1>
-				<ul class="steps font-mono">
-					<li class="step step-primary">Type</li>
-					<li class="step {step > 1 ? 'step-primary' : ''}">Info</li>
-					<li class="step {step > 2 ? 'step-primary' : ''}">Review</li>
-					<li class="step {step > 3 ? 'step-primary' : ''}">Copy</li>
-				</ul>
-				<div class="divider" />
-				{#if step === 1}
-					<SelectBusinessType bind:businessType />
-				{/if}
-				{#if step === 2}
-					<InputInfo bind:generatorType bind:businessType bind:businessName bind:businessEmail />
-				{/if}
-				{#if step === 3}
-					<ReviewDoc
-						bind:businessEmail
-						bind:businessName
-						bind:businessType
-						bind:generatorType
-						bind:policyMarkdown
-					/>
-				{/if}
-				{#if step === 4}
-					<DownloadDoc bind:policyMarkdown />
-				{/if}
-				<section class="w-full flex flex-col items-center">
-					{#if step > 1}
-						<div class="divider" />
-						<div class="flex flex-row justify-between w-full">
-							<button
-								class="btn btn-outline"
-								on:click={() => {
-									if (step === 4) {
-										step = 2;
-									} else {
-										handleBackward();
-									}
-								}}
-							>
-								{step === 2 ? 'Start Over' : 'Edit Info'}
-							</button>
-							<button
-								class="btn btn-primary hover:btn-success"
-								on:click={() => {
-									if (step === 4) {
-										resetForm();
-									} else {
-										handleForward();
-									}
-								}}
-							>
-								{nextButtonText}
-							</button>
-						</div>
-					{/if}
-				</section>
-			</div>
-		</div>
+		<GeneratorWindow bind:generatorType />
 	</div>
 </div>
